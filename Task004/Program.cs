473489eeccc4d410665bdec0c617940b9e4db7aa
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-25 14:06:48 +0300

    added tesk004

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+Console.WriteLine("Введите число A:");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C:");
+int userNumberC = Convert.ToInt32(Console.ReadLine());
+int max = userNumberA;
+if (max < userNumberB) max = userNumberB;
+if (max < userNumberC) max = userNumberC;
+Console.WriteLine(max);
